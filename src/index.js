497201cef3d6475c28a38afb66c46885c3cd0895
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-05 19:43:01 +0300

    Import useEffect hook to fetch GitHub user's data.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { useState, useEffect } from 'react';
 import reportWebVitals from './reportWebVitals';
+
+function App(){
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+    .then(response => response.json())
+    .then(setData)
+
+  },[]);
+  if(data) {
+    return (
+      <div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+      <button onClick={() => setData([])}>Remove Data</button>
+      </div>
+    )
+  }
+  return <p>No Users</p>
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +36,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
